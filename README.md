daa57c2144fba4f094578d07d17f01e5c00fcdac
Teruaki.Sato <andrea.pirlo.0529@gmail.com>
2024-10-03 22:17:55 +0900

    初期処理(versionコマンド)

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # api-forge
-OpenAPIを入力に様々作成するCLI
+
+`openapi.yaml`に記載された各種情報を抽出するコマンドラインツール
+
+## Install
+
+Githubのリリースサイトから、各環境用の圧縮ファイルをDownloadし任意のフォルダに展開.<br>
+コマンドラインから利用可能.
+
+[最新版のリリースページはコチラ](https://github.com/teru-0529/api-forge/releases/latest)
---
+++ cmd/root.go
@@ -0,0 +1,76 @@
+/*
+Copyright © 2024 Teruaki Sato <andrea.pirlo.0529@gmail.com>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	version     string
+	releaseDate string
+)
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "api-forge",
+	Short: "generate service from openapi specification.",
+	Long:  "generate service from openapi specification.",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+// FUNCTION:
+func Execute(ver string, date string) {
+	version = ver
+	releaseDate = date
+
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+// FUNCTION:
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// PROCESS:サブコマンドの追加
+	rootCmd.AddCommand(versionCmd)
+
+	// TODO:cofigファイルの定義(viper)は未整備
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.api-forge.yaml)")
+
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+// TODO:cofigファイルの定義(viper)は未整備
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".api-forge" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".api-forge")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
